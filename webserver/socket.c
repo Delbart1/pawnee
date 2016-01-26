3ac7d5a9ef4ceef9b58b47d66f8524b4de12a4b2
Jonathan DELBART <jonathan.delbart@etudiant.univ-lille1.fr>
2016-01-26 11:51:45 +0100

    modif socket.c

--- webserver/socket.c
+++ webserver/socket.c
@@ -1,23 +1,41 @@
-#include <unistd.h>
-#include <stdio.h>
-#include <sys/types.h>  
+#include <sys/types.h>
 #include <sys/socket.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <arpa/inet.h>
 
-int creer_serveur(int port){
-	port = 8080;
-	int socket_serveur ;
-	socket_serveur = socket ( AF_INET , SOCK_STREAM , 0);
-	if (socket_serveur == -1)
+int creer_serveur(int port)
+{
+
+	int socket_serveur = socket(AF_INET, SOCK_STREAM, 0);
+	if(socket_serveur == -1)
 	{
 		perror("socket_serveur");
 		return -1;
 	}
 
-	if(close(socket_serveur) == -1){
-		perror("close socket");
+	struct sockaddr_in saddr;
+	saddr.sin_family = AF_INET;
+	saddr.sin_port = htons (port);
+	saddr.sin_addr.s_addr = INADDR_ANY;
+	if(bind(socket_serveur, (struct sockaddr *)&saddr, sizeof(saddr)) == -1)
+	{
+		perror ("bind socket_serveur");
 		return -1;
 	}
-	/* Utilisation de la socket serveur */
-	port ++;
+
+	if(listen(socket_serveur, 10) == -1)
+	{
+		perror("listen socket_serveur");
+		return -1;
+	}
+
+	if(close(socket_serveur) == -1)
+	{
+		perror("close socket_serveur");
+		return -1;
+	}
+
 	return 0;
 }
+
