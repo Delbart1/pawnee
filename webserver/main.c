ac499f2dba1a94a299019bf30093cb0d4e215d30
Jonathan DELBART <jonathan.delbart@etudiant.univ-lille1.fr>
2016-02-02 10:46:36 +0100

    lecture/ecriture des messages du clients grace au serveur

--- webserver/main.c
+++ webserver/main.c
@@ -4,23 +4,47 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <stdlib.h>
+
 int main(/*int argc, char **argv*/void)
 {
+
+	printf("pawnee");
 	int socket_client;
 	int socket_serveur = creer_serveur(8080);
 
-	socket_client = accept(socket_serveur, NULL, NULL);
-
-	if(socket_client == -1)
+	
+	while(1)
 	{
-		perror("accept");
-		return -1;	
-	}
+	
+		socket_client = accept(socket_serveur, NULL, NULL);
 
+		if(socket_client == -1)
+		{
+			perror("accept");
+			return -1;	
+		}
+		
+		char * buffer = malloc(300);
+
+		
+		const char *message_bienvenue = "Bonjour, bienvenue sur mon serveur\n";
+		write(socket_client,message_bienvenue, strlen(message_bienvenue));
+
+		while(1)
+		{
+			if(read(socket_client,buffer,sizeof(buffer)) == -1)
+			{
+				perror("read client");
+				return -1;
+			}
 	
-	const char *message_bienvenue = "Bonjour, bienvenue sur mon serveur\n";
-	write(socket_client,message_bienvenue, strlen(message_bienvenue));
+			write(socket_client,buffer, sizeof(buffer));
+		}
 	
+	
+ 	}
+
 	if(close(socket_client) ==-1)
 	{
 		perror("close");
