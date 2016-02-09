946a3d2688794504b3272c26ddf7b759c03d0c0e
Jonathan DELBART <jonathan.delbart@etudiant.univ-lille1.fr>
2016-02-09 10:28:17 +0100

    ajout REUSEADDR

--- webserver/socket.c
+++ webserver/socket.c
@@ -7,8 +7,14 @@
 
 int creer_serveur(int port)
 {
+		int opt_val = 1;
+		int socket_serveur = socket(AF_INET, SOCK_STREAM, 0);
 
-	int socket_serveur = socket(AF_INET, SOCK_STREAM, 0);
+		if(setsockopt(socket_serveur, SOL_SOCKET, SO_REUSEADDR, &opt_val , sizeof(int)) == -1)
+	{
+		perror("Can not set SO_REUSEADDR option");
+		return -1;
+	}
 
 	if(socket_serveur == -1)
 	{
