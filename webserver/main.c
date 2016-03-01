634930227d798d71cc852374f3317774ce07a5bc
Jonathan DELBART <jonathan.delbart@etudiant.univ-lille1.fr>
2016-03-01 11:58:14 +0100

    debut  5.2.2.1

--- webserver/main.c
+++ webserver/main.c
@@ -10,13 +10,13 @@
 int main(/*int argc, char **argv*/void)
 {
 
-	printf("pawnee");
+	printf("Bienvenu sur pawnee\n");
 	int socket_client;
 	int socket_serveur = creer_serveur(8080);
 	FILE* stream;
 	while(1)
 	{
-		int end = 1;
+		
 		socket_client = accept(socket_serveur, NULL, NULL);
 		stream = fdopen (socket_client ,"w+");
 		if(socket_client == -1)
@@ -33,20 +33,17 @@ int main(/*int argc, char **argv*/void)
 			const char *message_bienvenue = "Bonjour, bienvenue sur mon serveur \n 1\n 2\n 3\n 4\n 5\n 6\n 7\n 8\n 9\n 10\n";
 			sleep(1);
 			fprintf(stream,"%s",message_bienvenue);
-			
-			while(end == 1)
-			{
-
+		
 				int taille = 256 * sizeof(char);
 				char * buffer = malloc(taille);
 				
 				while(fgets(buffer, taille, stream) != '\0')
 				{
 			
-					fprintf(stream,"%s %s","<Pawnee>",buffer);
-					end = 0;
+					printf("%s %s","<Pawnee>",buffer);
+				
 				}	
-			}	
+				
 		}
 		else
 		{
